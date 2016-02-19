5ac7a09d62633f90edb75c523f3baa3644225b40
LeonFerreirax <leon_stm@hotmail.com>
2016-02-19 01:40:49 -0200

    Update ufd_showBoundingBoxes.m

--- ufd_showBoundingBoxes.m
+++ ufd_showBoundingBoxes.m
@@ -1,12 +1,14 @@
-function ufd_showBoundingBoxes(X,boxes)
-% function ufd_showBoundingBoxes(X,boxes)
+function ufd_showBoundingBoxes(X,boxes)% function ufd_showBoundingBoxes(X,boxes)
 %Show the boxes as red rectangles. Simpler than the result of
 %the commands below in Matlab:
-% faceDetector = vision.CascadeObjectDetector;
-% I = imread('visionteam.jpg');
-% bboxes = step(faceDetector, I);
-% IFaces = insertObjectAnnotation(I, 'rectangle', bboxes, 'Face');
-% figure, imshow(IFaces), title('Detected faces');
+red = uint8([255 0 0]);
+shapeInserter = vision.ShapeInserter('Shape','Rectangles','BorderColor','Custom', 'CustomBorderColor', red);
+faceDetector = vision.CascadeObjectDetector; % faceDetector = vision.CascadeObjectDetector;
+I = imread('faces1.jpg');% I = imread('visionteam.jpg');
+RGB = repmat(I,[1,1,3]);
+bboxes = step(faceDetector, RGB);% bboxes = step(faceDetector, I);
+IFaces = insertObjectAnnotation(RGB, shapeInserter, bboxes,'Face');% IFaces = insertObjectAnnotation(I, 'rectangle', bboxes, 'Face');
+figure, imshow(IFaces), title('Detected faces');% figure, imshow(IFaces), title('Detected faces');
 
 imshow(X)
 %TO DO: show the boxes superimposed to the image
